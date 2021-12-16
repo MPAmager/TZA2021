4132b48cb41fde49100a1a55eb0ff57fbb4c974c
bipsen <ipsen.bertil@gmail.com>
2021-12-16 16:54:28 +0100

    Updated insta script

--- convert_addresses_to_insta_locations.py
+++ convert_addresses_to_insta_locations.py
@@ -1,23 +1,21 @@
+"""
+This should be a json file containing a list of addresses, eg. from the
+find_addresses_in_area.py script """
+
 import json
 import instaloader
 from tqdm.autonotebook import tqdm
 
 
 with open('addresses.json', 'r') as f:
-    addresses = json.load(f)  # This should be a json file containing a list of addresses
+    addresses = json.load(f)
 
 L = instaloader.Instaloader()
 
 locations = {}
 for place in tqdm(addresses):
     search = instaloader.TopSearchResults(L.context, place)
-    g = search.get_locations()
-
-    sub_locs = []  # Each place may return multiple locations
-    while True:
-        loc = next(g)
-        sub_locs.append(loc)
-
+    sub_locs = [loc for loc in search.get_locations()]
     locations[place] = sub_locs
 
 with open('locations.json', 'w') as f:
